fb020966789e02374f6c59627875ceebae6a6463
Anatholy Bricon <anatholyb@gmail.com>
2025-02-14 01:58:11 +0100

    add auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
+"use client";
 import Image from "next/image";
+import { useSession, signIn } from "next-auth/react";
 
 export default function Home() {
+
+  const { status } = useSession();
+
+  if(status === "loading") {
+    return (
+      <div className="grid place-items-center min-h-screen">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if(status === "unauthenticated") {
+    return (
+      signIn()
+    );
+  }
+
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
