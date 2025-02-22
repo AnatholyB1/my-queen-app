6cdd16108208d92facc2898a2fd9eab4992a5fdb
anatholyb1 <anatholyb@gmail.com>
2025-02-22 13:46:06 +0100

    firebase connection issue fixed

--- backEnd/firebaseNotification.ts
+++ backEnd/firebaseNotification.ts
@@ -1,12 +1,41 @@
 "use server";
 import admin from "firebase-admin";
 import { Message } from "firebase-admin/messaging";
-import serviceAccount from "@/adminSdk.json";
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  });
+const serviceAccountBase = process.env.FIREBASE_SERVICE_ACCOUNT;
+if (!serviceAccountBase) {
+  throw new Error("Missing FIREBASE_SERVICE_ACCOUNT environment variable");
+}
+
+const fireBaseId = process.env.FIREBASE_ID;
+if (!fireBaseId) {
+  throw new Error("Missing FIREBASE_ID environment variable");
+}
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: "queen-app-417ef",
+  private_key_id: fireBaseId,
+  private_key: serviceAccountBase,
+  client_email:
+    "firebase-adminsdk-fbsvc@queen-app-417ef.iam.gserviceaccount.com",
+  client_id: "118160915009649317984",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40queen-app-417ef.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com",
+};
+
+try {
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    });
+  }
+} catch (error) {
+  console.log("Firebase admin initialization error", (error as Error).message);
 }
 
 export const SendNotification = async ({
@@ -37,6 +66,6 @@ export const SendNotification = async ({
 
     return { success: true, message: "Notification sent!" };
   } catch (error) {
-    return { success: false, error };
+    return { success: false, error: (error as Error).message };
   }
 };
