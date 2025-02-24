343508963ec846686934912cdf52bce941309a85
anatholyb1 <anatholyb@gmail.com>
2025-02-24 21:53:54 +0100

    add return info to user on button main

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,31 @@ import { Button } from "@/components/ui/button";
 import { useMutation } from "@tanstack/react-query";
 import { SendNotification } from "@/backEnd/firebaseNotification";
 import { useSession } from "next-auth/react";
+import { LoaderCircle } from "lucide-react";
+import { toast } from "sonner";
 
 export default function Home() {
   const { data: session } = useSession();
   const email = session?.user?.email;
 
-  const { mutate: server_SendNotification } = useMutation({
+  const { mutate: server_SendNotification, isPending } = useMutation({
     mutationFn: SendNotification,
+    onSuccess: (success) => {
+      if (success) {
+        toast.success("Notification sent");
+      } else {
+        toast.error("Notification failed");
+      }
+    },
+    onError: () => {
+      toast.error("Notification failed");
+    },
   });
 
   if (!email) {
     return <div>loading...</div>;
   }
+
   const body = {
     user: email,
     title: "Don't ignore me",
@@ -22,14 +35,22 @@ export default function Home() {
     link: "/",
   };
 
+  const Content = () => {
+    if (isPending) {
+      return <LoaderCircle className="animate-spin !w-full !h-full p-4" />;
+    } else {
+      return <>Give me attention</>;
+    }
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
       <Button
         type="button"
-        className="rounded-full w-32 h-32 hover:scale-150"
+        className="rounded-full w-32 h-32 transform transition-transform duration-300 active:scale-150"
         onClick={() => server_SendNotification(body)}
       >
-        Give me attention
+        <Content />
       </Button>
     </div>
   );
